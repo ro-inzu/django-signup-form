626e8fab0c5e32513574a9dc3c47045659741280
Roinzunza <rosendoinzunza@.com>
2020-05-10 19:38:05 -0700

    created new app and index view

--- signupform/views.py
+++ signupform/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django import forms
+from django.http import HttpResponse
 
 # Create your views here.
+def index(requests):
+
+    return HttpResponse("Hello, world. You're at the signupform index.")

e9fd52cfaee6596f7ed3cf123ca43f8afe8076ce
Roinzunza <rosendoinzunza@.com>
2020-05-11 20:50:40 -0700

    updated settings for django support

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+           'ENGINE': 'djongo',
+           'NAME': 'your-db-name',
+       }
 }
 
 
